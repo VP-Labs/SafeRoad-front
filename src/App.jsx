19871abe0a678ff449f858ecba5687d5a4ac90f1
viclem <viclem1614@gmail.com>
2024-09-29 15:28:43 +0200

    ajout de la page Home

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import PrivateRoute from './pages/PrivateRoute'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Questionnaire from './pages/Questionnaire'
 import Error from './pages/Error'
 import './App.css'
 
@@ -21,6 +22,7 @@ function App() {
 	          <Route path="/signup" element={<Signup />} />
 	          <Route element={<PrivateRoute />}>
 	              <Route path="/" element={<Home />} />
+	              <Route path="/questionnaire" element={<Questionnaire />} />
 	          </Route>
 	          <Route path="*" element={<Error />} />
 	      </Routes>
